c93a15c60706ed5425a4275089c6c7169058cb35
Neil-98 <35454571+Neil-98@users.noreply.github.com>
2022-05-09 20:53:14 -0400

    Update network.py
    Added short_text_capsule_net() and long_text_capsule_net() based on Goldani et. al., "Detecting Fake News with Capsule Neural Networks".

--- network.py
+++ network.py
@@ -74,3 +74,43 @@ def capsule_model_A(X, num_classes):
         nets = capsule_flatten(nets)
         poses, activations = capsule_fc_layer(nets, num_classes, 3, 'fc2') 
     return poses, activations
+
+def short_text_capsule_model(X, num_classes):
+    poses_list = []
+    for _, ngram in enumerate([3, 5]):
+        with tf.variable_scope('capsule_'+str(ngram)): 
+            nets = _conv2d_wrapper(
+                X, shape=[ngram, 300, 1, 32], strides=[1, 2, 1, 1], padding='VALID', 
+                add_bias=True, activation_fn=tf.nn.relu, name='conv1'
+            )
+            tf.logging.info('output shape: {}'.format(nets.get_shape()))
+            nets = capsules_init(nets, shape=[1, 1, 32, 16], strides=[1, 1, 1, 1], 
+                                 padding='VALID', pose_shape=16, add_bias=True, name='primary')                        
+            nets = capsule_conv_layer(nets, shape=[3, 1, 16, 16], strides=[1, 1, 1, 1], iterations=3, name='conv2')
+            nets = capsule_flatten(nets)
+            poses, activations = capsule_fc_layer(nets, num_classes, 3, 'fc2')
+            poses_list.append(poses)
+    
+    poses = tf.reduce_mean(tf.convert_to_tensor(poses_list), axis=0) 
+    activations = K.sqrt(K.sum(K.square(poses), 2))
+    return poses, activations
+
+def long_text_capsule_model(X, num_classes):
+    poses_list = []
+    for _, ngram in enumerate([2, 3, 4, 5]):
+        with tf.variable_scope('capsule_'+str(ngram)): 
+            nets = _conv2d_wrapper(
+                X, shape=[ngram, 300, 1, 32], strides=[1, 2, 1, 1], padding='VALID', 
+                add_bias=True, activation_fn=tf.nn.relu, name='conv1'
+            )
+            tf.logging.info('output shape: {}'.format(nets.get_shape()))
+            nets = capsules_init(nets, shape=[1, 1, 32, 16], strides=[1, 1, 1, 1], 
+                                 padding='VALID', pose_shape=16, add_bias=True, name='primary')                        
+            nets = capsule_conv_layer(nets, shape=[3, 1, 16, 16], strides=[1, 1, 1, 1], iterations=3, name='conv2')
+            nets = capsule_flatten(nets)
+            poses, activations = capsule_fc_layer(nets, num_classes, 3, 'fc2')
+            poses_list.append(poses)
+    
+    poses = tf.reduce_mean(tf.convert_to_tensor(poses_list), axis=0) 
+    activations = K.sqrt(K.sum(K.square(poses), 2))
+    return poses, activations
